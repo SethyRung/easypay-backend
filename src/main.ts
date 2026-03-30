eea935dc147f72463252b84c134c5b2fc8f32995
Sethy Rung <rungsethyhk@gmail.com>
2026-03-30 08:13:18 +0700

    feat: add global validation pipe for request validation

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from "@nestjs/core";
 import { ConfigService } from "@nestjs/config";
+import { ValidationPipe } from "@nestjs/common";
 import { AppModule } from "@/app.module";
 import { setupSwagger } from "@/config/swagger.config";
 
@@ -12,6 +13,17 @@ async function bootstrap() {
     origin: config.get("CORS_ORIGIN"),
     credentials: true,
   });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+      forbidNonWhitelisted: true,
+    }),
+  );
 
   setupSwagger(app);
 
